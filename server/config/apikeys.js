708edb2414284ed5ad090c6b2eb123fab4218c49
lylynguyen <lylynguyen812@gmail.com>
2016-01-13 14:00:21 -0800

    got modal to work

--- server/config/apikeys.js
+++ server/config/apikeys.js
@@ -1,6 +1,19 @@
+// module.exports = {
+//   googleOauth: {
+//     clientID: "456598125555-0nbiekk5rdp1lup82j6hmirp47inhkq1.apps.googleusercontent.com",
+//     clientSecret: "IFl-oxG-LGoazw9tmJiavmjl"
+//   }
+// }
+
+
 module.exports = {
+
   googleOauth: {
-    clientID: "456598125555-0nbiekk5rdp1lup82j6hmirp47inhkq1.apps.googleusercontent.com",
-    clientSecret: "IFl-oxG-LGoazw9tmJiavmjl"
+
+    clientID: '334378260452-lvf0cn3dap30og2nivblcmkvfl7qq0me.apps.googleusercontent.com',
+
+    clientSecret: 'aMXdozZo-Sp8igyOeD0bk8uV'
+
   }
+
 }
